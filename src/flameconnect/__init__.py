b1e0e32750b3130cc677dcc49a20741446f53962
Andrew Berry <andrew@furrypaws.ca>
2026-03-11 14:02:07 -0400

    chore(main): release 0.6.0

--- src/flameconnect/__init__.py
+++ src/flameconnect/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.5.3"
+__version__ = "0.6.0"
 
 from flameconnect.auth import AbstractAuth, MsalAuth, TokenAuth
 from flameconnect.client import FlameConnectClient
